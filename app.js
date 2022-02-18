d4dd374857e03039486e352728a47c2cbbd1bd22
DAVID HERRERA COSTA, DAVID <11233535@alu.murciaeduca.es>
2022-02-18 17:04:45 +0100

    creado el db.js y los metodos get de ambos

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
 require("dotenv").config();
-const express = require("express");
-const mongo = require("mongodb");
-const MongoClient = mongo.MongoClient;
-const MONGO_URI = process.env.MONGO;
 const PUERTO = process.env.PORT || process.env.PUERTO;
+const db = require("./db.js");
 
+const express = require("express");
 const app = express();
 app.use(express.json());
 
+
+
 //Me conecto a la base de datos
-MongoClient.connect(MONGO_URI, function (err, db) {
-  if (err) throw err;
-  coleccionPokemon = db.db("db").collection("pokemon");
-  app.listen(PORT, () =>
+//MongoClient.connect(MOONGOSE, function (err, db) {
+  //if (err) throw err;
+  //coleccionPokemon = db.db("db").collection("pokemon");
+  app.listen(PUERTO, () =>
     console.log(`Servicio escuchando en el puerto: ${PUERTO}`)
   );
-});
+//});
--- db.js
+++ db.js
@@ -1,5 +1,97 @@
-//operaciones relacionadas con la base de datos
+const mongoose = require("mongoose");
+require("dotenv").config();
 
-recuperar(); //get
+const PokemonSchema = new mongoose.Schema({
+  tipo: { type: String, required: true },
+  naturaleza: { type: String, required: true },
+  nombre: { type: String, required: true },
+  ataque: { type: Number, required: true },
+  defensa: { type: Number, required: true },
+  nivel: { type: Number, required: true },
+  apodo: { type: String, required: false},
+});
+
+const EntrenadorSchema = new mongoose.Schema({
+  edad: { type: Number, required: true }, //ojo number quiero integer
+  apellido: { type: String, required: false },
+  nombre: { type: String, required: true },
+  medallas: { type: Number, required: true },
+  gorra: { type: Boolean, required: false },
+  ciudadNacimiento: {type: String, required: false},
+});
+
+const Pokemon = mongoose.model("Pokemon", PokemonSchema);
+const Entrenador = mongoose.model("Entrenador", EntrenadorSchema);
+
+exports.Entrenador = Entrenador;
+exports.Pokemon = Pokemon;
+
+exports.connect = async function () {
+  mongoose.connect(process.env.MONGOOSE); //MI BASE DE DATOS
+};
+
+exports.disconnect = mongoose.disconnect;
+
+exports.encontrarPokemons = async function () { //hacer exports.encontrarPokemons
+  return await Pokemon.find({});
+}; //get todos los pokemons
+
+const encontrarPokemonPorId = async function (idPokemon) {
+  return await Pokemon.findOne({ _id: idPokemon });
+};
+
+exports.guardarPokemonEnBD = async function (pokemon) {
+  await pokemon.save();
+};
+
+exports.borrarPokemonDeBD = async function (idPokemon) {
+  await Pokemon.deleteOne({ _id: idPokemon });
+};
+
+exports.actualizarPokemon = async function (idPokemon, pokemon) {
+  var actualizarPokemon = await encontrarPokemonPorId(idPokemon);
+  actualizarPokemon.tipo = pokemon.tipo; //ojo este Pokemon es como el de schema cuidado, usar el de parametro
+  actualizarPokemon.naturaleza = pokemon.naturaleza;
+  actualizarPokemon.nombre = pokemon.nombre;
+  actualizarPokemon.ataque = pokemon.ataque;
+  actualizarPokemon.defensa = pokemon.defensa;
+  actualizarPokemon.nivel = pokemon.nivel;
+  actualizarPokemon.apodo = pokemon.apodo;
+  await actualizarPokemon.save();
+};
+
+exports.encontrarEntrenador = async function () {
+  return await Entrenador.find({});
+};
+
+const encontrarEntrenadorPorId = async function (idEntrenador) {
+  return await Entrenador.findOne({ _id: idEntrenador });
+};
+
+exports.guardarEntrenadorEnBD = async function (entrenador) {
+  await entrenador.save();
+};
+
+exports.borrarEntrenadorDeBD = async function (idEntrenador) {
+  await Entrenador.deleteOne({ _id: idEntrenador });
+};
+
+exports.actualizarEntrenador = async function (idEntrenador, entrenador) {
+  var actualizarEntrenador = await encontrarEntrenadorPorId(idEntrenador);
+  actualizarEntrenador.edad = entrenador.edad;
+  actualizarEntrenador.apellido = entrenador.apellido;
+  actualizarEntrenador.nombre = entrenador.nombre;
+  actualizarEntrenador.medallas = entrenador.medallas;
+  actualizarEntrenador.gorra = entrenador.gorra;
+  actualizarEntrenador.ciudadNacimiento = entrenador.ciudadNacimiento;
+  await actualizarEntrenador.save();
+};
+
+
+//exports
+//exports.encontrarPokemons = encontrarPokemons; este hecho antes
+exports.encontrarPokemonPorId = encontrarPokemonPorId;
+exports.encontrarEntrenadorPorId = encontrarEntrenadorPorId;
+
 insertar(); //post
 borrar(); //delete
